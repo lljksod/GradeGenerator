91a97a083a3cd38d0400c05a52f4555b1512d79d
Richie Burch <rburch@gmail.com>
2019-03-05 16:43:35 -0600

    Initial commit

---
+++ app/src/main/java/com/example/gradegenerator/GradeActivity.java
@@ -0,0 +1,13 @@
+package com.example.gradegenerator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GradeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/gradegenerator/StudentMasterFragment.java
@@ -0,0 +1,9 @@
+package com.example.gradegenerator;
+
+import android.support.v4.app.Fragment;
+import android.widget.TextView;
+
+public class StudentMasterFragment extends Fragment {
+
+    TextView sn = 
+}
